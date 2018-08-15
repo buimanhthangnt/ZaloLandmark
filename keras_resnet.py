a3d946e762d168de9147b14aed60877ea8b40068
Bui Manh Thang <buimanhthangnt@gmail.com>
2018-08-15 23:39:56 +0700

    validation in training

--- keras_resnet.py
+++ keras_resnet.py
@@ -6,6 +6,7 @@ import pickle
 from sklearn.utils import shuffle
 import tensorflow as tf
 import cv2
+from sklearn.model_selection import train_test_split
 
 
 num_classes = 103
@@ -13,15 +14,16 @@ num_epochs = 3
 batch_size = 128
 
 model = ResNet50(weights='imagenet', include_top=False)
+x_train, y_train, _ = pickle.load(open('data.pickle', 'rb'))
+x_train, y_train = shuffle(x_train, y_train)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.08, random_state=12)
 
 
-def next_batch(batch_size=batch_size):
-    x_train, y_train, _ = pickle.load(open('data.pickle', 'rb'))
-    x_train, y_train = shuffle(x_train, y_train)
-    num_batch = len(y_train) // batch_size
+def next_batch(XXX, YYY, batch_size=batch_size):
+    num_batch = len(YYY) // batch_size
     for i in range(num_batch):
         x_batch, y_batch = [], []
-        for path, label in zip(x_train[i*batch_size:(i+1)*batch_size], y_train[i*batch_size:(i+1)*batch_size]):
+        for path, label in zip(XXX[i*batch_size:(i+1)*batch_size], YYY[i*batch_size:(i+1)*batch_size]):
             try:
                 img = image.load_img(path, target_size=(224, 224))
             except:
@@ -49,34 +51,35 @@ def get_features(images):
     return features
 
 
-x_train = tf.placeholder(dtype=tf.float32, shape=[None, 7, 7, 2048])
-y_train = tf.placeholder(dtype=tf.float32, shape=[None, num_classes])
+X = tf.placeholder(dtype=tf.float32, shape=[None, 7, 7, 2048])
+y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes])
 
-pred = tf.layers.average_pooling2d(x_train, (7, 7), strides=(7,7))
+pred = tf.layers.average_pooling2d(X, (7, 7), strides=(7,7))
 pred = tf.layers.flatten(pred)
 pred = tf.layers.dense(pred, num_classes)
 
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_train, logits=pred))
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=pred))
 train_op = tf.train.AdamOptimizer(learning_rate=0.0005).minimize(loss_op)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(num_epochs):
         j = 0
-        for x_feed, y_feed in next_batch():
-            loss, _ = sess.run([loss_op, train_op], feed_dict={x_train: x_feed, y_train: y_feed})
+        for x_feed, y_feed in next_batch(x_train, y_train):
+            loss, _ = sess.run([loss_op, train_op], feed_dict={X: x_feed, y: y_feed})
             if j % 50 == 0:
                 print("Epoch %d, batch %d: loss = %f" % (i, j, loss))
+            if j % 300 == 0:
+                accs = []
+                for x_val_feed, y_val_feed in next_batch(x_val, y_val):
+                    y_pred = sess.run(pred, feed_dict={X: x_val_feed, y: y_val_feed})
+                    y_pred = np.argmax(y_pred, axis=1)
+                    y_val_label = np.argmax(y_val_feed, axis=1)
+                    acc = np.mean(y_pred == y_val_label)
+                    accs.append(acc)
+                print("Validation accuracy: %f" % np.mean(np.array(accs)))
             j += 1
     
     saver = tf.train.Saver()
     saver.save(sess, './model/resnet.ckpt')
-    print("Saved")
-
-
-# img = image.load_img('/media/thangbm/6CF2096DF2093CB8/Images/Wallpapers/1.jpg', target_size=(224, 224))
-# img = image.img_to_array(img)
-# img = np.expand_dims(img, 0)
-# features = get_features(img)
-# print(features.shape)
-# print('Predicted:', decode_predictions(preds, top=3)[0])
+    print("Saved model")
