6e8523a71ca8f2c219bd1a260778401ca4cf77c1
Bui Manh Thang <buimanhthangnt@gmail.com>
2018-08-18 00:38:51 +0700

    optimize code test

--- test.py
+++ test.py
@@ -1,89 +1,53 @@
 from __future__ import division
 import os
 import numpy as np
-import glob
-import cv2
 import pickle
-import cv2
-from sklearn.utils import shuffle
-from keras.preprocessing import image
-from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from keras.applications.inception_resnet_v2 import preprocess_input
-from keras.applications.resnet50 import ResNet50
-from keras.applications.resnet50 import preprocess_input
 import csv
 import tensorflow as tf
 import config
 
 
-model = ResNet50(weights='imagenet', include_top=False)
+mode = 'res'
+feature_shape = None
+
+if mode == 'res':
+    feature_shape = [None, 7, 7, 2048]
+elif mode == 'inc':
+    feature_shape = [None, 8, 8, 1536]
+elif mode == 'v3':
+    feature_shape = [None, 8, 8, 2048]
 
 
-def get_features(images):
-    global model
-    features = model.predict(images)
-    return features
-
-
-def get_test(path):
-    x = []
-    for image in os.listdir(path):
-        x.append(os.path.join(path, image))
-    return np.array(x)
-
-
-def next_batch_test(XXX, batch_size=config.batch_size):
+def next_batch_test():
     path = 'pickle_test'
     for fil in os.listdir(path):
         x_batch, paths = pickle.load(open(os.path.join(path, fil), 'rb'))
         yield x_batch, paths
 
 
-    # num_batch = int(np.ceil(len(x) / config.batch_size))
-    # for i in range(num_batch):
-    #     x_batch, paths = [], []
-    #     for path in x[i*config.batch_size:(i+1)*config.batch_size]:
-    #         try:
-    #             img = image.load_img(path, target_size=(config.image_size, config.image_size))
-    #         except:
-    #             print(path)
-    #             continue
-
-    #         img = image.img_to_array(img)
-    #         x_batch.append(img)
-
-    #         path = path.split('/')[1].split('.')[0]
-    #         paths.append(path)
-
-    #     x_batch = np.array(x_batch)
-    #     x_batch = preprocess_input(x_batch)
-    #     x_batch = get_features(x_batch)
-
-    #     paths = np.array(paths)
-    #     pickle.dump((x_batch, paths), open('pickle_test/' + str(i) + '.pickle', 'wb'), pickle.HIGHEST_PROTOCOL)
-    #     # yield x_batch, paths
-
-
 _, _, x_test = pickle.load(open('data.pickle', 'rb'))
-# next_batch_test(x_test)
-# exit(0)
 
+X = tf.placeholder(dtype=tf.float32, shape=feature_shape)
 
-X = tf.placeholder(dtype=tf.float32, shape=config.feature_shape)
+pred = None
+if mode == 'res':
+    pred = tf.layers.average_pooling2d(X, (7, 7), strides=(7,7))
+    pred = tf.layers.flatten(pred)
+elif mode == 'inc':
+    pred = tf.reduce_mean(X, axis=[1,2])
+elif mode == 'v3':
+    pred = tf.reduce_mean(X, axis=[1,2])
 
-# pred = tf.reduce_mean(X, axis=[1,2])
-pred = tf.layers.average_pooling2d(X, (7, 7), strides=(7,7))
-pred = tf.layers.flatten(pred)
 pred = tf.layers.dense(pred, config.num_classes)
 
 sess =  tf.Session()
 sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 
-saver.restore(sess, './model2/resnet.ckpt')
+saver.restore(sess, './model_' + mode + '/' + mode + '.ckpt')
 results = []
 ids = []
-for x_batch, paths in next_batch_test(x_test):
+for x_batch, paths in next_batch_test():
     prediction = sess.run(pred, feed_dict={X: x_batch})
     prediction = np.argsort(prediction, axis=1)
     prediction = prediction[:,::-1]
@@ -97,7 +61,11 @@ content.append(title)
 for col1, col2 in zip(ids, results):
     col2 = ' '.join(str(ele) for ele in col2)
     content.append([col1, col2])
-result_file = open('results_res.csv', 'w')
+
+save_path = 'results/'
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
+result_file = open(os.path.join(save_path, mode + '.csv'), 'w')
 with result_file:
     writer = csv.writer(result_file)
     writer.writerows(content)
