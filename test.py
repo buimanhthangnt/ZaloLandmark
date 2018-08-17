eb47973c1afb7ac86507c760f5894f56235fb4e8
Bui Manh Thang <buimanhthangnt@gmail.com>
2018-08-17 18:54:27 +0700

    ok

--- test.py
+++ test.py
@@ -69,7 +69,7 @@ pred = tf.layers.flatten(pred)
 pred = tf.layers.dense(pred, config.num_classes)
 
 sess =  tf.Session()
-sess.run(tf.global_variables_initializer())
+# sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 
 saver.restore(sess, config.resnet_model)
