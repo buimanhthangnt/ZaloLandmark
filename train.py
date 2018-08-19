f4799232df53492db74d1cbeb2717450411ee306
Bui Manh Thang <buimanhthangnt@gmail.com>
2018-08-19 19:54:56 +0700

    fix bug

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ pred = utils.top_layers(X, mode)
 pred = tf.layers.dense(pred, config.num_classes)
 
 # Weight loss
-class_weights = tf.cast(tf.constant(class_weights), tf.float32)
+class_weights = tf.cast(tf.constant([class_weights]), tf.float32)
 weight_per_label = tf.transpose(tf.matmul(y, tf.transpose(class_weights)))
 sum_loss = tf.multiply(weight_per_label, \
                        tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=pred))
