45e79814335d8b224d3f643841b474c5517a6815
Bui Manh Thang <buimanhthangnt@gmail.com>
2018-08-22 22:33:44 +0700

    okay

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 num_classes = 103
 num_epochs = 200
-batch_size = 256
+batch_size = 128
 patience = 36
-learning_rate = 0.00012
+learning_rate = 0.0005
 image_size_res = 224
 image_size_inc = 299
 image_size_v3 = 299
